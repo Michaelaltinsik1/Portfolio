a10bda210ed389d49f383165d6ce562bc964fc68
Michael.altinisik1 <Michael.altinisik@iths.se>
2023-10-08 19:15:42 +0200

    fix url

--- src/components/Features/YoutubeVideo.tsx
+++ src/components/Features/YoutubeVideo.tsx
@@ -8,7 +8,7 @@ const YoutubeVideo = ({ videoId, title }: YoutubeVideoProps) => {
     <iframe
       className="w-full h-[200px] tablet:h-[400px] my-auto rounded-lg desktop:h-[500px]"
       src={
-        'https://www.youtube.com/embed/' + videoId + '?autoplay=1?enablejsapi=1'
+        'https://www.youtube.com/embed/' + videoId + '?autoplay=1&enablejsapi=1'
       }
       title={`${title}`}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
