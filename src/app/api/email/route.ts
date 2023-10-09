7a4beab42df209da71465536c6465b8924002600
Michael.altinisik1 <Michael.altinisik@iths.se>
2023-10-09 12:20:25 +0200

    Add html template for nodemailer submission

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -7,7 +7,6 @@ const password = process.env.NODEMAILER_PASS;
 
 export async function POST(request: NextRequest) {
   const { email, name, message } = await request.json();
-
   const transport = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -20,7 +19,43 @@ export async function POST(request: NextRequest) {
     from: nodemailer_email,
     to: nodemailer_email,
     subject: `Message from ${name} (${email})`,
-    text: message,
+    html: `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Your Email Subject</title>
+        <style>
+            /* Add CSS styles here */
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #EADBC8;
+                margin: 0;
+                padding: 0;
+            }
+            .container {
+                margin: 0 auto;
+                padding: 20px;
+                background-color: #EADBC8;
+                border-radius: 5px;
+            }
+            h1 {
+                color: #0F2C59;
+            }
+            p {
+                color: #0F2C59;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Message received from ${name}!</h1>
+            <p>${name} has the following email address: ${email}</p>
+            <p>${message}</p>
+        </div>
+    </body>
+    </html>
+    `,
   };
 
   const sendMailPromise = () =>
