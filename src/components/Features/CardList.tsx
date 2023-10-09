459cad1d4dad423f27e497b35afbaec22aa31c86
Michael.altinisik1 <Michael.altinisik@iths.se>
2023-10-09 12:35:21 +0200

    Remove placeholder image and add space torusim landing page image

--- src/components/Features/CardList.tsx
+++ src/components/Features/CardList.tsx
@@ -1,11 +1,11 @@
 import Card from '@/features/Card';
 import projects from '@/utils/projects.json';
-import crew from '../../../public/space-crew.jpg';
+import space from '../../../public/space-tourism.jpg';
 import ems from '../../../public/ems.jpg';
 import gardening from '../../../public/puutarhuri.jpg';
 import acupuncture from '../../../public/akupunktur.jpg';
 const CardList = () => {
-  const images = [ems, gardening, crew, acupuncture];
+  const images = [ems, gardening, space, acupuncture];
   return (
     <div className="mt-[24px] desktop:mt-[40px]">
       {projects.map((project, index) => (
